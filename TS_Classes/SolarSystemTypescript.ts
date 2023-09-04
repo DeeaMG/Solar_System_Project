2c5c26dd774b7fc09cf35fbbc3c03e75fb5c3420
Ganea Andreea Maria <ganea.andreea.maria@gmail.com>
2023-09-04 20:20:40 +0300

    Typescript Solar System

--- TS_Classes/SolarSystemTypescript.ts
+++ TS_Classes/SolarSystemTypescript.ts
@@ -1,11 +1,13 @@
 class Entity {
   constructor(
-    name: string,
+    public name: string,
     private radius: number,
     private mass: number,
     protected canvas: HTMLCanvasElement,
     protected context: CanvasRenderingContext2D,
-    protected angleOwnAxis: number
+    protected angleOwnAxis: number,
+    private x: number = canvas.width / 2,
+    private y: number = canvas.height / 2
   ) {}
 
   rotateAroundOwnAxis() {
@@ -39,148 +41,114 @@ class Entity {
     return this.mass;
   }
 
-  get x() {
-    return this.canvas.width / 2;
+  get getX() {
+    return this.x;
   }
 
-  get y() {
-    return this.canvas.height / 2;
+  get getY() {
+    return this.y;
+  }
+
+  set setX(newX: number) {
+    this.x = newX;
+  }
+
+  set setY(newY: number) {
+    this.y = newY;
   }
 }
 
-class BlackHole {
-  private radius!: number;
-  private mass!: number;
-  name: string;
+class BlackHole extends Entity {
   angularSpeed: number;
   angle: number;
-  x: number;
-  y: number;
 
-  constructor(name: string) {
-    this.name = name;
+  constructor(name: string, canvas: HTMLCanvasElement, context: CanvasRenderingContext2D) {
+    super(name, 0, 0, canvas, context, 0);
     this.angularSpeed = 0.001;
     this.angle = 0;
-    this.x = canvas.width / 2;
-    this.y = canvas.height / 2;
-  }
-
-  setRadius(newRadius: number) {
-    this.radius = newRadius;
-  }
-
-  getRadius() {
-    return this.radius;
-  }
-
-  setMass(newMass: number) {
-    this.mass = newMass;
-  }
-
-  getMass() {
-    return this.mass;
   }
 
   createBlackHole() {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    context.beginPath();
-    context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
-    context.fillStyle = "black";
-    context.fill();
-    context.closePath();
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.context.beginPath();
+    this.context.arc(this.getX, this.getY, this.getRadius(), 0, 2 * Math.PI);
+    this.context.fillStyle = "black";
+    this.context.fill();
+    this.context.closePath();
 
-    context.font = "bold 40px Arial";
-    context.fillStyle = "white";
-    context.textAlign = "center";
-    context.textBaseline = "middle";
-    context.fillText(this.name, this.x, this.y);
+    this.context.font = "bold 40px Arial";
+    this.context.fillStyle = "white";
+    this.context.textAlign = "center";
+    this.context.textBaseline = "middle";
+    this.context.fillText(this.name, this.getX, this.getY);
     requestAnimationFrame(this.createBlackHole.bind(this));
   }
 }
 
-class Star {
-  private radius!: number;
-  private mass!: number;
+class Star extends Entity {
   name: string;
   speedOwnAxis: number;
   distanceFromBlackHole: number;
   color: string;
   type: string;
   angleOwnAxis: number;
-  x: number;
-  y: number;
 
-  constructor(name: string, speed: number, distanceBlackHole: number) {
+  constructor(
+    name: string,
+    speed: number,
+    distanceBlackHole: number,
+    canvas: HTMLCanvasElement,
+    context: CanvasRenderingContext2D
+  ) {
+    super(name, 0, 0, canvas, context, 0);
     this.name = name;
     this.speedOwnAxis = speed;
     this.distanceFromBlackHole = distanceBlackHole;
     this.color = "yellow";
     this.type = "Yellow Dwarf Star";
     this.angleOwnAxis = 0;
-    this.x = canvas.width / 2;
-    this.y = canvas.height / 2;
-  }
-
-  setRadius(newRadius: number) {
-    this.radius = newRadius;
-  }
-
-  getRadius() {
-    return this.radius;
-  }
-
-  setMass(newMass: number) {
-    this.mass = newMass;
-  }
-
-  getMass() {
-    return this.mass;
   }
 
   createStar() {
-    context.beginPath();
-    context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
-    context.fillStyle = this.color;
-    context.fill();
-    context.closePath();
+    this.context.beginPath();
+    this.context.arc(this.getX, this.getY, this.getRadius(), 0, 2 * Math.PI);
+    this.context.fillStyle = this.color;
+    this.context.fill();
+    this.context.closePath();
 
-    context.font = "bold 30px Arial";
-    context.fillStyle = "black";
-    context.textAlign = "center";
-    context.textBaseline = "middle";
-    context.fillText(this.name, this.x, this.y);
+    this.context.font = "bold 30px Arial";
+    this.context.fillStyle = "black";
+    this.context.textAlign = "center";
+    this.context.textBaseline = "middle";
+    this.context.fillText(this.name, this.getX, this.getY);
 
-    const dotX = this.x + Math.cos(this.angleOwnAxis) * (this.radius - 15);
-    const dotY = this.y + Math.sin(this.angleOwnAxis) * (this.radius - 15);
+    const dotX = this.getX + Math.cos(this.angleOwnAxis) * (this.getRadius() - 15);
+    const dotY = this.getY + Math.sin(this.angleOwnAxis) * (this.getRadius() - 15);
 
-    context.beginPath();
-    context.arc(dotX, dotY, 3, 0, 2 * Math.PI);
-    context.fillStyle = "black";
-    context.fill();
-    context.closePath();
+    this.context.beginPath();
+    this.context.arc(dotX, dotY, 3, 0, 2 * Math.PI);
+    this.context.fillStyle = "black";
+    this.context.fill();
+    this.context.closePath();
   }
 
   rotateAroundOwnAxis() {
-    context.clearRect(0, 0, canvas.width, canvas.height);
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     this.angleOwnAxis += this.speedOwnAxis;
     const angle = this.angleOwnAxis + (this.distanceFromBlackHole / 100) * (2 * Math.PI);
-    this.x = canvas.width / 2 + Math.cos(angle) * this.distanceFromBlackHole;
-    this.y = canvas.height / 2 + Math.sin(angle) * this.distanceFromBlackHole;
+    this.setX = this.canvas.width / 2 + Math.cos(angle) * this.distanceFromBlackHole;
+    this.setY = this.canvas.height / 2 + Math.sin(angle) * this.distanceFromBlackHole;
     this.createStar();
     requestAnimationFrame(this.rotateAroundOwnAxis.bind(this));
   }
 }
 
-class Planet {
-  private radius!: number;
-  private mass!: number;
+class Planet extends Entity {
   name: string;
   color: string;
   speedOwnAxis: number;
   speedSunAxis: number;
   distanceFromSun: number;
-  x: number;
-  y: number;
   angleOwnAxis: number;
   trajectory: number;
   sun: Star;
@@ -192,57 +160,42 @@ class Planet {
     speedAroundSun: number,
     distanceSun: number,
     trajectory: number,
-    sun: Star
+    sun: Star,
+    canvas: HTMLCanvasElement,
+    context: CanvasRenderingContext2D
   ) {
+    super(name, 0, 0, canvas, context, 0);
     this.name = name;
     this.color = color;
     this.speedOwnAxis = speed;
     this.speedSunAxis = speedAroundSun;
     this.distanceFromSun = distanceSun;
-    this.x = canvas.width / 2;
-    this.y = canvas.height / 2;
     this.angleOwnAxis = 0;
     this.trajectory = trajectory;
     this.sun = sun;
   }
 
-  setRadius(newRadius: number) {
-    this.radius = newRadius;
-  }
-
-  getRadius() {
-    return this.radius;
-  }
-
-  setMass(newMass: number) {
-    this.mass = newMass;
-  }
-
-  getMass() {
-    return this.mass;
-  }
-
   createPlanet() {
-    context.beginPath();
-    context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
-    context.fillStyle = this.color;
-    context.fill();
-    context.closePath();
+    this.context.beginPath();
+    this.context.arc(this.getX, this.getY, this.getRadius(), 0, 2 * Math.PI);
+    this.context.fillStyle = this.color;
+    this.context.fill();
+    this.context.closePath();
 
-    context.font = "bold 30px Arial";
-    context.fillStyle = "black";
-    context.textAlign = "center";
-    context.textBaseline = "middle";
-    context.fillText(this.name, this.x, this.y);
+    this.context.font = "bold 30px Arial";
+    this.context.fillStyle = "black";
+    this.context.textAlign = "center";
+    this.context.textBaseline = "middle";
+    this.context.fillText(this.name, this.getX, this.getY);
 
-    const dotX = this.x + Math.cos(this.angleOwnAxis) * this.radius;
-    const dotY = this.y + Math.sin(this.angleOwnAxis) * this.radius;
+    const dotX = this.getX + Math.cos(this.angleOwnAxis) * this.getRadius();
+    const dotY = this.getY + Math.sin(this.angleOwnAxis) * this.getRadius();
 
-    context.beginPath();
-    context.arc(dotX, dotY, 3, 0, 2 * Math.PI);
-    context.fillStyle = "black";
-    context.fill();
-    context.closePath();
+    this.context.beginPath();
+    this.context.arc(dotX, dotY, 3, 0, 2 * Math.PI);
+    this.context.fillStyle = "black";
+    this.context.fill();
+    this.context.closePath();
   }
 
   rotateAroundAxis() {
@@ -253,8 +206,8 @@ class Planet {
 
   rotateAroundSun() {
     const angle = this.angleOwnAxis + this.distanceFromSun * (2 * Math.PI);
-    this.x = this.sun.x + Math.cos(angle) * this.distanceFromSun;
-    this.y = this.sun.y + Math.sin(angle) * this.distanceFromSun;
+    this.setX = this.sun.getX + Math.cos(angle) * this.distanceFromSun;
+    this.setY = this.sun.getY + Math.sin(angle) * this.distanceFromSun;
     this.createPlanet();
     requestAnimationFrame(this.rotateAroundSun.bind(this));
   }
@@ -265,29 +218,29 @@ class SolarSystem {
   sun: Star;
   planets: Planet[];
 
-  constructor() {
+  constructor(canvas: HTMLCanvasElement, context: CanvasRenderingContext2D) {
     const centerX = canvas.width / 2;
     const centerY = canvas.height / 2;
     const blackHoleRadius = 500;
 
-    const blackhole = new BlackHole("The Blackest Black Hole");
+    const blackhole = new BlackHole("The Blackest Black Hole", canvas, context);
     blackhole.setRadius(300);
     blackhole.setMass(100000);
 
     this.blackHole = blackhole;
-    this.sun = new Star("The Sun", 0.01, 700);
+    this.sun = new Star("The Sun", 0.01, 700, canvas, context);
     this.sun.setRadius(200);
     this.sun.setMass(1000);
 
     this.planets = [
-      new Planet("Mercury", "#8a4317", 0.002, 0.001, 300, 10, this.sun),
-      new Planet("Venus", "#fa7705", 0.004, 0.002, 500, 20, this.sun),
-      new Planet("Earth", "#f25b29", 0.006, 0.003, 700, 30, this.sun),
-      new Planet("Mars", "#f25b29", 0.008, 0.004, 900, 20, this.sun),
-      new Planet("Jupiter", "#3263a8", 0.003, 0.005, 1100, 30, this.sun),
-      new Planet("Saturn", "#e8b941", 0.005, 0.006, 1300, 20, this.sun),
-      new Planet("Uranus", "#329ba8", 0.007, 0.007, 1500, 30, this.sun),
-      new Planet("Neptune", "#3263a8", 0.009, 0.008, 1700, 20, this.sun),
+      new Planet("Mercury", "#8a4317", 0.002, 0.001, 300, 10, this.sun, canvas, context),
+      new Planet("Venus", "#fa7705", 0.004, 0.002, 500, 20, this.sun, canvas, context),
+      new Planet("Earth", "#f25b29", 0.006, 0.003, 700, 30, this.sun, canvas, context),
+      new Planet("Mars", "#f25b29", 0.008, 0.004, 900, 20, this.sun, canvas, context),
+      new Planet("Jupiter", "#3263a8", 0.003, 0.005, 1100, 30, this.sun, canvas, context),
+      new Planet("Saturn", "#e8b941", 0.005, 0.006, 1300, 20, this.sun, canvas, context),
+      new Planet("Uranus", "#329ba8", 0.007, 0.007, 1500, 30, this.sun, canvas, context),
+      new Planet("Neptune", "#3263a8", 0.009, 0.008, 1700, 20, this.sun, canvas, context),
     ];
 
     for (let i = 0; i < this.planets.length; i++) {
@@ -307,8 +260,8 @@ class SolarSystem {
       context.clearRect(0, 0, canvas.width, canvas.height);
       blackHole.createBlackHole();
       const angle = star.angleOwnAxis + (star.distanceFromBlackHole / radius) * (2 * Math.PI);
-      star.x = centerX + Math.cos(angle) * star.distanceFromBlackHole;
-      star.y = centerY + Math.sin(angle) * star.distanceFromBlackHole;
+      star.setX = centerX + Math.cos(angle) * star.distanceFromBlackHole;
+      star.setY = centerY + Math.sin(angle) * star.distanceFromBlackHole;
       star.createStar();
       requestAnimationFrame(animate);
     };
@@ -337,5 +290,8 @@ class SolarSystem {
   }
 }
 
-const solarSystem = new SolarSystem();
+const canvas = document.getElementById("circleCanvas") as HTMLCanvasElement;
+const context = canvas.getContext("2d") as CanvasRenderingContext2D;
+
+const solarSystem = new SolarSystem(canvas, context);
 solarSystem.rotatePlanets();
